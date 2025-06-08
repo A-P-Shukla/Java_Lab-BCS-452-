0cfa183af7d4edd41dfd4a7425a7094513de8a54
Akhand Pratap Shukla <145320759+A-P-Shukla@users.noreply.github.com>
2025-06-08 16:58:45 +0530

    Create exp1.java

---
+++ exp1.java
@@ -0,0 +1,26 @@
+public class EscapeDemo { 
+public static void main(String[] args) { 
+// Basic message 
+System.out.println("Hello, Java World from Eclipse!"); 
+ 
+// Using newline (\n) 
+System.out.println("\nThis is printed on a new line."); 
+ 
+// Using tab (\t) 
+System.out.println("Name:\tJohn Doe"); 
+System.out.println("Roll:\t101"); 
+ 
+// Using backslash (\\) 
+System.out.println("File path: C:\\Program Files\\Java\\jdk"); 
+ 
+// Using double quote (\") 
+System.out.println("She said, \"Java is awesome!\""); 
+ 
+// Using single quote (\') 
+System.out.println("It\'s a good day to learn Java."); 
+ 
+// Using carriage return (\r) 
+System.out.println("Learning Java is fun!\rStart: "); 
+// Output may vary depending on the console; \r returns to the beginning of the line. 
+} 
+} 
